cc3210e3cbf28190ebdc7ef3aa699ecfa58b42d9
Lauri Piispanen <lauri.piispanen@reaktor.com>
2017-12-05 22:50:21 +0200

    update generator version

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "github-top" ]
 then
-  curl -L -O https://github.com/lauripiispanen/most-active-github-users-counter/releases/download/v1.1/github-top
+  curl -L -O https://github.com/lauripiispanen/most-active-github-users-counter/releases/download/v1.2/github-top
   chmod u+x github-top
 fi
 
