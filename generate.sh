33272d48c92c755755e8aa1bd151b76d0fe9c97e
Lauri Piispanen <lauri.piispanen@reaktor.com>
2019-09-22 20:49:14 +0300

    bump client version

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "github-top" ]
 then
-  curl -L -O https://github.com/lauripiispanen/most-active-github-users-counter/releases/download/v1.10/github-top.cgo_disabled
+  curl -L -O https://github.com/lauripiispanen/most-active-github-users-counter/releases/download/v1.11/github-top.cgo_disabled
   mv github-top.cgo_disabled github-top
   chmod u+x github-top
 fi
