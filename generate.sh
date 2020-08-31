4a85e2c346f0a73f299db9c487f08f6f773531b4
Lauri Piispanen <lauri.piispanen@reaktor.com>
2020-08-31 21:28:51 +0300

    output all three page modes

--- generate.sh
+++ generate.sh
@@ -12,8 +12,7 @@ OUTPUT_FILE_NAME=$(echo "$2" | sed 's/ /_/')
 
 ./github-top --token "$1" --preset "$2" --output yaml --file "$OUTPUT_FILE_NAME.yml"
 
-if [ -f "$OUTPUT_FILE_NAME.yml" ]; then
-  echo "page: $OUTPUT_FILE_NAME.html\ntitle: $3" | cat - "$OUTPUT_FILE_NAME.yml" > "_data/locations/$OUTPUT_FILE_NAME.yml"
-  echo "---\ntype: location\nlocation: $OUTPUT_FILE_NAME\npublic: true\n---" > "$OUTPUT_FILE_NAME.md"
-  echo "---\ntype: location\nlocation: $OUTPUT_FILE_NAME\nprivate: true\n---" > "${OUTPUT_FILE_NAME}_private.md"
-fi
+echo "page: $OUTPUT_FILE_NAME.html\ntitle: $3" | cat - "$OUTPUT_FILE_NAME.yml" > "_data/locations/$OUTPUT_FILE_NAME.yml"
+echo "---\ntype: location\nlocation: $OUTPUT_FILE_NAME\nmode: commits\n---" > "$OUTPUT_FILE_NAME.md"
+echo "---\ntype: location\nlocation: $OUTPUT_FILE_NAME\nmode: all\n---" > "${OUTPUT_FILE_NAME}_private.md"
+echo "---\ntype: location\nlocation: $OUTPUT_FILE_NAME\nmode: contributions\n---" > "${OUTPUT_FILE_NAME}_public.md"
